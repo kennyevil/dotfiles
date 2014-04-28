ec2bd9eb7a46452f14560c9dd737407ddeab203e
Peter Rhoades <createdbypete@gmail.com>
2014-04-28 11:15:44 +0100

    No need for curl-ca-bundle

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -1,6 +1,5 @@
 # Homebrew exports
 if which brew &>/dev/null; then
-  export SSL_CERT_FILE="$(brew --prefix curl-ca-bundle)/share/ca-bundle.crt"
   export PATH="/usr/local/bin:$PATH"
   export CFLAGS="-I/usr/local/include"
   export LDFLAGS="-L/usr/local/lib"
